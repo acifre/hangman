19de904f949e9930cdf6c19e1d71704c13170dec
acifre <78503928+acifre@users.noreply.github.com>
2022-12-22 23:08:39 -0500

    Add check_won and player wins logic

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -48,6 +48,10 @@ class Game
 
     @start_game = false
 
+    @incorrect_guesses = []
+
+    @player_won = false
+
   end
 
   def get_user_input
@@ -57,28 +61,43 @@ class Game
     end
     input
   end
-
   def add_underscores(times)
     times.times do
       @display += "_"
     end
   end
+  def display_info
 
+    puts "\nNumber of guesses left: #{@number_of_guesses}.\n"
+    if @incorrect_guesses.length > 1
+      puts "Incorrect guesses: #{@incorrect_guesses.join(" ")}\n"
+    elsif @incorrect_guesses.length == 1
+      puts "Incorrect guesses: #{@incorrect_guesses.first}\n"
+    else
+      puts "Incorrect guesses: NONE\n"
+    end
+    puts "\n"
+    puts @display
+    puts "\n"
+
+  end
   def get_user_guesses
     guess = get_user_input
     if @word_to_guess.include?(guess)
       index_array = @word_to_guess.indexes(guess)
       index_array.each { |index| @display[index] = guess}
-      puts "CORRECT GUESS"
+      puts "\nCORRECT GUESS\n"
+      @player_won = check_won
+
     else
       @number_of_guesses -= 1
-      puts "WRONG GUESS"
+      @incorrect_guesses.push(guess)
+      puts "\nWRONG GUESS\n"
     end
 
-    puts @display
+    display_info
 
   end
-
   def welcome_message
     puts "Welcome to Hangman!\n"
     puts "You will have 6 tries to guess the word correctly.\n"
@@ -92,9 +111,12 @@ class Game
 
   end
 
+  def check_won
+    @word_to_guess == @display
+  end
 
   def launch_game
-
+    puts @word_to_guess
     # SETUP DISPLAY
     add_underscores(@length)
 
@@ -104,16 +126,19 @@ class Game
 
     if @start_game
       puts @display
-      while @number_of_guesses > 0
+      while @number_of_guesses > 0 && @player_won == false
         get_user_guesses
       end
-      if @number_of_guesses == 0
-        puts "GAME OVER"
+      if @number_of_guesses == 0 && @player_won == false
+        puts "\nGAME OVER\n"
+        puts "The word was #{@word_to_guess}.\n"
+      elsif @player_won
+        puts "\nPLAYER WON\n"
       end
       end
     end
 
-  # ask user prompt to guess letter
+
 end
 
 test_game = Game.new("test")
